637cceab50e144f6b530481445c341feea9407c9
policarpofelipe <policarpofelipe@gmail.com>
2026-03-06 17:48:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PI1 UNIVESP</title>
+  <title>PI1 UNIVESP 2026</title>
 </head>
 <body>
   <h1>PI1 - Sistema Web de Controle de Estoque</h1>
