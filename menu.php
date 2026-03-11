9af8f1c9a1aa58bae19f69fab7594b8f46bb96b3
policarpofelipe <policarpofelipe@gmail.com>
2026-03-11 10:31:32 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -17,7 +17,7 @@ $menuPrincipal = [
     [
         'url'   => 'relatorio_estoque_baixo.php',
         'label' => 'Estoque Baixo',
-        'icon'  => 'M0 0h24v24H0zM4 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0M15 17h-9v-14h-2M6 5l14 1l-.854 5.976m-2.646 1.024h-10.5M19 16v3M19 22v.01',
+        'icon'  => 'M4 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0 M15 17h-9v-14h-2 M6 5l14 1l-.854 5.976m-2.646 1.024h-10.5 M19 16v3 M19 22v.01',
     ],
 ];
 
